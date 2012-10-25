e40ae62cc7eb4e91e053d5aae196c8c6dd4684b7
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-25 16:13:39 +0200

    Added lint task, it works

--- grunt.js
+++ grunt.js
@@ -1,13 +1,56 @@
 module.exports = function(grunt) {
 
+  // Configuration of the tasks we use
   grunt.initConfig({
-    dosth: { default: {} }
+    // Config for the dummy task, to show how it works
+    dummy: { test: [ 1, 2, 4, 8, 16 ] }
+
+    // JSHint configuration
+    // see here for explanation: http://www.jshint.com/options/
+  , jshint: {
+      options: { browser: true
+               , curly: true
+               , devel: true
+               , debug: true
+               , eqeqeq: true
+               , expr: true
+               , forin: true
+               , immed: true
+               , latedef: true
+               , laxcomma: true
+               , newcap: true
+               , noarg: true
+               , onevar: true
+               , trailing: true
+               , undef: true
+               }
+    , globals: { // needed for requireJS
+                 define: true
+               , require: true
+               , requirejs: true
+               , has: true
+               , module: true
+               // needed for accessing env variables in gruntfile
+               , process: true
+               }
+    }
+
+    // The lint task will run JSHint and report any errors
+    // You can change the options for this task, by reading this:
+    // https://github.com/cowboy/grunt/blob/master/docs/task_lint.md
+  , lint: { requireConfig: ['assets/js/config.requirejs.js']
+          }
+
+
   });
 
-  grunt.registerMultiTask('dosth', 'Do something', function () {
+  // Example Grunt multi task
+  grunt.registerMultiTask('dummy', 'Do something', function () {
     console.log("Hello world");
+    console.log(this.target);
+    console.log(this.data);
   });
 
-  grunt.registerTask('default', 'dosth:default');
+  grunt.registerTask('default', 'dummy:test');
 
 };
